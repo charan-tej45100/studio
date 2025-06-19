1712b8bdfebb23e08f6bae03fad50ed558d84df0
THAMMISHETTY DEVI CHARAN TEJ <rkdctej18@gmail.com>
2025-06-19 14:39:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection>
    
    wit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        blink: 'blink 1.5s ease-in-out infinite',
+        blink: 'blink 0.75s ease-in-out infinite',
       },
     },
   },
